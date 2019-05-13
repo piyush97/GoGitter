e74a2574feea10178e212ede8e8cdc153d25671f
Piyush Mehta <me@piyushmehta.com>
2019-05-13 12:08:40 +0530

    solving webpack 4 problem with react

---
+++ src/index.js
@@ -0,0 +1,15 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+require("./index.css");
+//// Component depends on all this
+// State
+// Lifecycle event
+// UI
+
+class App extends React.Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+// Component and where to render
+ReactDOM.render(<App />, document.getElementById("app"));
