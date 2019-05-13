d6015720f5e542b8a59e25a54680cf6d38350e3f
Piyush Mehta <me@piyushmehta.com>
2019-05-13 23:36:01 +0530

    badge

--- app/index.js
+++ app/index.js
@@ -2,10 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class App extends React.Component {
+class Badge extends React.Component {
   render() {
-    return <div>Hello ReactTraining!</div>;
+    return (
+      <div>
+        <img
+          src={this.props.img}
+          alt="avatar"
+          style={{ width: 100, heigth: 100 }}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>username: {this.props.username}</h3>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <Badge
+    name="Piyush Mehta"
+    username="piyush97"
+    img="https://via.placeholder.com/300.png"
+  />,
+  document.getElementById("app")
+);
