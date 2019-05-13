57c79251da4dfc9b88217278e7d52baaa0af9c3c
Piyush Mehta <me@piyushmehta.com>
2019-05-13 13:20:36 +0530

    SOLVED FINALLY 😄

--- app/index.js
+++ app/index.js
@@ -1,15 +1,11 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-require("./index.css");
-//// Component depends on all this
-// State
-// Lifecycle event
-// UI
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class App extends React.Component {
   render() {
     return <div>Hello World!</div>;
   }
 }
-// Component and where to render
+
 ReactDOM.render(<App />, document.getElementById("app"));
