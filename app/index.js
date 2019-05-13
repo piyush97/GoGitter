c9c6b349a17c91f5d14b8d62fd3e307d1902f11c
Piyush Mehta <me@piyushmehta.com>
2019-05-13 11:17:15 +0530

    component know how

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 require("./index.css");
-
+//// Component depends on all this
 // State
 // Lifecycle event
 // UI
+
 class App extends React.Component {
   render() {
     return <div>Hello World!</div>;
