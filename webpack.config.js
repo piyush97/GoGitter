c0cad8d23a743642bfb50f96b43782e273492442
Piyush Mehta <me@piyushmehta.com>
2019-05-13 13:15:36 +0530

    config for webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,93 +1,22 @@
-const webpack = require("webpack");
 const path = require("path");
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-/*
- * We've enabled UglifyJSPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
- *
- */
-
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: "./app/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js"
+  },
   module: {
     rules: [
-      {
-        include: [path.resolve(__dirname, "src")],
-        loader: "babel-loader",
-
-        options: {
-          plugins: ["syntax-dynamic-import"],
-
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                modules: false
-              }
-            ]
-          ]
-        },
-
-        test: /\.js$/
-      },
-      {
-        test: /\.css$/,
-
-        use: [
-          {
-            loader: "style-loader",
-
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: "css-loader"
-          }
-        ]
-      }
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] }
     ]
   },
-
-  entry: {
-    index: "./app/index"
-  },
-
-  output: {
-    filename: "[name].[chunkhash].js"
-  },
-
   mode: "development",
-
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          priority: -10,
-          test: /[\\/]node_modules[\\/]/
-        }
-      },
-
-      chunks: "async",
-      minChunks: 1,
-      minSize: 30000,
-      name: true
-    }
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "app/index.html"
+    })
+  ]
 };
