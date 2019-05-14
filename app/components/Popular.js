ac7dd0ae3d7984929f2f547fa4bdf344f14b6090
Piyush Mehta <me@piyushmehta.com>
2019-05-14 17:36:08 +0530

    Popular

---
+++ app/components/Popular.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Popular extends Component {
+  render() {
+    return <div>Popular!</div>;
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,35 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import PropTypes from "prop-types";
+import App from "./components/App";
 
-class Badge extends React.Component {
-  render() {
-    return (
-      <div>
-        <img
-          src={this.props.img}
-          alt="avatar"
-          style={{ width: 100, heigth: 100 }}
-        />
-        <h1>Name: {this.props.name}</h1>
-        <h3>username: {this.props.username}</h3>
-      </div>
-    );
-  }
-}
-
-Badge.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired
-};
-
-ReactDOM.render(
-  <Badge
-    name="Piyush Mehta"
-    username="piyush97"
-    img="https://via.placeholder.com/300.png"
-  />,
-  document.getElementById("app")
-);
+ReactDOM.render(<App />, document.getElementById("app"));
