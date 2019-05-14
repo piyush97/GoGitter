125f554aabcfa5ce3c6673c94dd84f5415823f00
Piyush Mehta <me@piyushmehta.com>
2019-05-14 23:54:23 +0530

    Managing state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
+import api from "../utils/api";
 
 export default class Popular extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: "All"
+      selectedLanguage: "All",
+      repos: null
     };
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
   updateLanguage(lang) {
     this.setState(function() {
-      return { selectedLanguage: lang };
+      return { selectedLanguage: lang, repos: null };
     });
+    api.fetchPopularRepos(lang).then(
+      function(repos) {
+        this.setState({ repos: repos });
+      }.bind(this)
+    );
   }
 
   render() {
--- app/utils/api.js
+++ app/utils/api.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export default {
   fetchPopularRepos: function(language) {
-    var encodedURI = window.encodedURI(
+    var encodedURI = window.encodeURI(
       "https://api.github.com/search/repositories?q=stars:>1+language:" +
         language +
         "&sort=starts&order=desc&type=Repositories"
     );
-    return axios.get(encodeURI).then(function(response) {
+    return axios.get(encodedURI).then(function(response) {
       return response.data.items;
     });
   }
