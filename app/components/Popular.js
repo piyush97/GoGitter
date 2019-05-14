a1dff3857b9e42986cdf3c03dce0b95ba7161cad
Piyush Mehta <me@piyushmehta.com>
2019-05-14 17:46:21 +0530

    UI and state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
 
 export default class Popular extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectLanguage: "All"
+    };
+  }
   render() {
-    return <div>Popular!</div>;
+    var languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    return (
+      <ul className="languages">
+        {languages.map(function(lang) {
+          return <li key={lang}>{lang}</li>;
+        })}
+      </ul>
+    );
   }
 }
