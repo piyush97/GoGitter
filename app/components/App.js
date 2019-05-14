fa917b2e70c67d3e743c38c634e079ebc38b5e12
Piyush Mehta <me@piyushmehta.com>
2019-05-14 17:37:44 +0530

    rendering popular

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Popular from "./Popular";
 
 export default class App extends React.Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <Popular />
+      </div>
+    );
   }
 }
