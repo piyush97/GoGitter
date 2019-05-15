572e9f09883fdd93de3c6db0536bbc4570cb5b0a
Piyush Mehta <me@piyushmehta.com>
2019-05-15 13:16:26 +0530

    errored

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -7,6 +7,7 @@ class PlayerInput extends Component {
       username: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
@@ -16,6 +17,11 @@ class PlayerInput extends Component {
         username: value
       };
     });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.props.id, this.state.username);
   }
 
   render() {
