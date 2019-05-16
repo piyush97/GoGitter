f4fb312f0dea42ac4cf66f3609b53f0d94589cb1
Piyush Mehta <me@piyushmehta.com>
2019-05-16 21:05:10 +0530

    winner looser for user

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,6 +1,26 @@
-var React = require("react");
+import React from "react";
+import queryString from "query-string";
+import api from "../utils/api";
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    };
+  }
+  componentDidMount() {
+    var players = queryString.parse(this.props.location.search);
+    api
+      .battle([players.playerOneName, players.playerTwoName])
+      .then(function(results) {
+        console.log(results);
+      });
+  }
+
   render() {
     return <div>Results!</div>;
   }
