a659793fdebac02332b979dfad16d347a2d72e02
Piyush Mehta <me@piyushmehta.com>
2019-05-16 21:27:15 +0530

    player preview

---
+++ app/components/PlayerPreview.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+function PlayerPreview(props) {
+  return (
+    <div>
+      <div className="column">
+        <img
+          className="avatar"
+          src={props.avatar}
+          alt={"Avatar for " + props.username}
+        />
+        <h2 className="username">@{props.username}</h2>
+      </div>
+      {props.children}
+    </div>
+  );
+}
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired
+};
+
+export default PlayerPreview;
