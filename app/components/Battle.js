754594aab45b3acf3ad613fbfb64d0cbff76330a
Piyush Mehta <me@piyushmehta.com>
2019-05-16 21:30:36 +0530

    playerPreview modularized

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,30 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-
-function PlayerPreview(props) {
-  return (
-    <div>
-      <div className="column">
-        <img
-          className="avatar"
-          src={props.avatar}
-          alt={"Avatar for " + props.username}
-        />
-        <h2 className="username">@{props.username}</h2>
-      </div>
-      <button className="reset" onClick={props.onReset.bind(null, props.id)}>
-        Reset
-      </button>
-    </div>
-  );
-}
-
-PlayerPreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired
-};
+import PlayerPreview from "./PlayerPreview";
 
 class PlayerInput extends React.Component {
   constructor(props) {
