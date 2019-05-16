decee4565efa27061e33a003e5fcba4ecd86437a
Piyush Mehta <me@piyushmehta.com>
2019-05-16 16:54:03 +0530

    player preview

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-class PlayerInput extends React.Component {
+function PlayerPreview(props) {
+  return (
+    <div>
+      <div className="column">
+        <img
+          className="avatar"
+          src={props.avatar}
+          alt={"Avatar for" + props.username}
+        />
+        <h2 className="username">@{props.username}</h2>
+      </div>
+      <button className="reset" onClick={props.onReset.bind(null, props.id)}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+// PlayerPreview.propTypes = {
+//   avatar: PropTypes.string.isRequired,
+//   username: PropTypes.string.isRequired,
+//   onReset: PropTypes.function.isRequired
+// };
+
+class PlayerInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
