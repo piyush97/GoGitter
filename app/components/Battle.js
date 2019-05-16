9dc6acd50c7b61e57b57a04e588b6dedeb1579f6
Piyush Mehta <me@piyushmehta.com>
2019-05-16 17:01:50 +0530

    binding reset

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -96,6 +96,7 @@ export default class Battle extends React.Component {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
   handleSubmit(id, username) {
     this.setState(function() {
@@ -103,6 +104,14 @@ export default class Battle extends React.Component {
       newState[id + "Name"] = username;
       newState[id + "Image"] =
         "https://github.com/" + username + ".png?size=200";
+      return newState;
+    });
+  }
+  handleReset(id) {
+    this.setState(function() {
+      var newState = {};
+      newState[id + "Name"] = "";
+      newState[id + "Image"] = null;
       return newState;
     });
   }
