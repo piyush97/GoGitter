813c6bd8c281361578c265d6acb91221678f088e
Piyush Mehta <me@piyushmehta.com>
2019-05-16 22:17:47 +0530

    convention

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
   config.plugins.push(
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify("production")
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
       }
     }),
     new webpack.optimize.UglifyJsPlugin()
