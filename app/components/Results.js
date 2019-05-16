77c0d62be23630a6c7189d244f778f582b50797e
Piyush Mehta <me@piyushmehta.com>
2019-05-16 21:17:39 +0530

    getting ranked

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,7 +1,7 @@
 import React from "react";
 import queryString from "query-string";
 import api from "../utils/api";
-
+import { Link } from "react-router-dom";
 class Results extends React.Component {
   constructor(props) {
     super(props);
@@ -18,11 +18,46 @@ class Results extends React.Component {
       .battle([players.playerOneName, players.playerTwoName])
       .then(function(results) {
         console.log(results);
-      });
+      })
+      .then(
+        function(results) {
+          if (results === null) {
+            return this.setState({
+              error:
+                "Looks like there's an error. Check that both users exists on GitHub 😄",
+              loading: false
+            });
+          }
+          this.setState({
+            error: null,
+            winner: results[0],
+            loser: results[1],
+            loading: false
+          });
+        }.bind(this)
+      );
   }
 
   render() {
-    return <div>Results!</div>;
+    var error = this.state.error;
+    var winner = this.state.winner;
+    var loser = this.state.loser;
+    var loading = this.state.loading;
+
+    if (loading === true) {
+      return <p> Loading </p>;
+    }
+
+    if (error) {
+      return (
+        <div>
+          <p>{error}</p>
+          <Link to="/battle">Reset</Link>
+        </div>
+      );
+    }
+
+    return <div>{JSON.stringify(this.state)}</div>;
   }
 }
 
